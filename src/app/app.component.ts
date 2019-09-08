f154018623002e5db943824df6b8a7f50761a4c2
Jason Wroe <jason_mark_wroe@hotmail.com>
2019-09-08 20:33:45 +0100

    remove commented code and some formating.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { LocationModel } from './Location-Model';
 import { LocationPosition } from './locationPosition-Model';
 import { throwError, NEVER } from 'rxjs';
 import { PathModel } from './path-Model';
-import { PathLocationStrategy } from '@angular/common';
+//import { PathLocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -31,11 +31,6 @@ export class AppComponent implements OnInit {
     //this.nodes = [1,2,3];
     this.nodes = [1];
 
-    //this.locations[] 
-    //this.locations.push(new LocationModel(), { location: ( new LocationPosition(), { positionX: 30, positionY:30}), paths: null});
-
-//    var pm :  PathModel = { toLocation: 1, fromLocation : 1 };
-
     var lm : LocationModel = { location: ( new LocationPosition(), { positionX: 30, positionY:30}), paths : [], fromLocation: null, id: '1'};
 
     this.locations.push(lm);
@@ -47,17 +42,16 @@ export class AppComponent implements OnInit {
     this.canvas.hoverCursor = 'pointer';
   }
 
-    someFunction(data){
+  someFunction(data){
 
-      var lm : LocationModel = { location: ( new LocationPosition(), { positionX: 30, positionY:80}), paths : [], fromLocation: null, id: "1"};
+    var lm : LocationModel = { location: ( new LocationPosition(), { positionX: 30, positionY:80}), paths : [], fromLocation: null, id: "1"};
 
-      this.locations[0]= lm;
+    this.locations[0]= lm;
 
-      this.canvas.clear();
+    this.canvas.clear();
 
 
     // resize the canvas
-//    this.canvas.setWidth( (this.nodes.length + 1 )* 200);
     this.canvas.setWidth( (this.locations.length + 1 )* 230);
     this.canvas.setHeight( 500 );
     this.canvas.calcOffset();
@@ -68,8 +62,6 @@ export class AppComponent implements OnInit {
 
     if (data.paths.length == 0){
       var pm :  PathModel = { toLocation: lm, fromLocation : data , hazard: 'something', dir: 0};
-
-  //    lm.paths.push(pm);
       data.paths.push(pm);
     } else if (data.paths.length == 1){
       var pm :  PathModel = { toLocation: lm, fromLocation : data , hazard: 'something', dir: 1};
@@ -80,25 +72,24 @@ export class AppComponent implements OnInit {
       //lm.location.positionY -=50;
       this.adjustChildren(lm.fromLocation);
     }
-    
+  
     this.locations.push(lm);
 
-//    this.nodes.push(this.nodes.length + 1);
     console.log(data);
   }
 
   adjustChildren(location: LocationModel) {
 
-      if (location)
-      {
-        location.location.positionY +=80;
+    if (location)
+    {
+      location.location.positionY +=80;
 
-        this.adjustChildren(location.fromLocation);
+      this.adjustChildren(location.fromLocation);
 
-      }else{
-        
-        return;
-      }
+    }else{
+      
+      return;
+    }
   }
 
 }
--- src/app/location-node/location-node.component.ts
+++ src/app/location-node/location-node.component.ts
@@ -19,30 +19,18 @@ export class LocationNodeComponent implements OnInit {
 
   @ViewChild('myDiv', {static:false}) myDiv: ElementRef;
 
-  circle2: fabric.Circle;
+  circle: fabric.Circle;
 
   constructor() { }
 
-  ngOnInit() {
-//    this.parentCanvas.add(new fabric.IText('Hello Fabric!'+this.id));
+    ngOnInit() {
 
-    /*var circle2 = new fabric.Circle({
-      radius: 30,
-      fill: '#e8e490',
-      left: 30  + (this.id * 200),
-      top:30,
-      opacity: 0.7,
-      stroke: '#e6bd4e',
-      strokeWidth: 3,
-      strokeUniform: true
-    });*/
-
-    if (this.circle2 == null)
+    if (this.circle == null)
     {
-      this.circle2 = new fabric.Circle({
+      this.circle = new fabric.Circle({
         radius: 30,
         fill: '#e8e490',
-        left: this.location.location.positionX  /*+ (this.id * 200)*/,
+        left: this.location.location.positionX,
         top: this.location.location.positionY,
         opacity: 0.7,
         stroke: '#e6bd4e',
@@ -74,7 +62,7 @@ export class LocationNodeComponent implements OnInit {
     this.parentCanvas.add(rect);
 
 
-    this.parentCanvas.add(this.circle2);
+    this.parentCanvas.add(this.circle);
 
     this.parentCanvas.add(text);
     
@@ -88,15 +76,11 @@ export class LocationNodeComponent implements OnInit {
   }
 
   ngAfterViewInit() {
-    
-    //this.myDiv.nativeElement.style.left = 30 + 'px';
-    //this.myDiv.nativeElement.style.top= 30 + 'px';
-
+  
     console.log(this.myDiv); 
   }
 
   _sendDataToParent() {
-    //this.sendDataToParent.emit(this.id);
     this.sendDataToParent.emit(this.location);
   }
 
